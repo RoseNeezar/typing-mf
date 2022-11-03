1b25ea7f33e0ba88277e2f4efb5b2f97bc866a59
Neezar <rforlyffe@gmaicl.com>
2022-11-03 23:04:09 +0800

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { SocketClient } from "./util/promiseSocket";
+
 function App() {
+  const events = useMemo(() => new SocketClient(), []);
+
+  const { status, data, error, isFetching } = useQuery(
+    ["test"],
+    async () =>
+      await events.postMessage("create-game", {
+        news: "hoho",
+      })
+  );
+
   return (
-    <div className="bg-purple-400 text-lg h-screen w-screen flex flex-row">
-      <div className="w-52 h-screen bg-purple-100">
+    <div className="flex flex-row w-screen h-screen text-lg bg-purple-400">
+      <div className="h-screen bg-purple-100 w-52">
         {/* {useBound.getState().navigate} */}
       </div>
-      <div className="mt-10  ">
+      <div className="mt-10 ">
         <div id="game" />
       </div>
     </div>
