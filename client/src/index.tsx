7c2bb5342fc6b1f3a232cf7388eea0793083724a
Neezar <rforlyffe@gmaicl.com>
2022-11-06 20:38:06 +0800

    react 18

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
-
+const el = document.getElementById("root") as HTMLElement;
+const root = createRoot(el);
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -17,15 +18,14 @@ const queryClient = new QueryClient({
   },
 });
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
